f2bf98dbec7f4cda1810df35cd90527118490278
steven42069 <stevendiaz152@gmail.com>
2025-09-30 00:33:31 -0400

    done

--- app/main.py
+++ app/main.py
@@ -68,7 +68,7 @@ class DeliveryDrone(FlyingRobot):
 
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load: Cargo = None
+        self.current_load: Cargo | None = None
 
         if current_load is not None:
             self.hook_load(current_load)
